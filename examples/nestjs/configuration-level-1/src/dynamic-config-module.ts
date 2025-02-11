f754b43c874c6a3582bc7f2cc1bcd32903871777
kuzyuberdin <kuzyuberdin@gmail.com>
2025-02-11 14:02:06 +0400

    fix env config

--- examples/nestjs/configuration-level-1/src/dynamic-config-module.ts
+++ examples/nestjs/configuration-level-1/src/dynamic-config-module.ts
@@ -1,12 +1,13 @@
 import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
 
 // you must import this const in the head of your app.module.ts
 export const configModule = ConfigModule.forRoot({
   envFilePath: [
     process.env.ENV_FILE_PATH?.trim() || '',
-    `.env.${process.env.NODE_ENV}.local`,
-    `.env.${process.env.NODE_ENV}`, // и могут быть переопределены выше стоящими файлами
-    '.env.production', // сначала берутся отсюда значение
+    join(__dirname, `env`, `.env.${process.env.NODE_ENV}.local`),
+    join(__dirname, `env`, `.env.${process.env.NODE_ENV}`), // и могут быть переопределены выше стоящими файлами
+    join(__dirname, `env`, '.env.production'), // сначала берутся отсюда значение
   ],
   isGlobal: true,
 });
