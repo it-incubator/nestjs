c95f82e20df72f3354c557e7572fdca3b1c9ccdf
dimych <kuzyuberdin@gmail.com>
2025-04-21 19:22:04 +0400

    add NODE_ENV for migrations and checking

--- examples/nestjs/typeorm-postgresql-migrations/src/env-file-paths.ts
+++ examples/nestjs/typeorm-postgresql-migrations/src/env-file-paths.ts
@@ -1,6 +1,10 @@
 // you must import this const in the head of your app.module.ts
 import { join } from 'path';
 
+if (!process.env.NODE_ENV) {
+  throw new Error('NODE_ENV is required');
+}
+
 export const envFilePaths = [
   process.env.ENV_FILE_PATH?.trim() || '',
   join(__dirname, `env`, `.env.${process.env.NODE_ENV}.local`),
